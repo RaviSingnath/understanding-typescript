bbb15ea12e91504997ea99ac41b91e2c23b25d8f
RaviSingnath <ravisingnath@gmail.com>
2023-07-08 13:43:38 +0530

    Using Interfaces with Classes

--- src/app.ts
+++ src/app.ts
@@ -1,139 +1,45 @@
-abstract class Department {
-    // public name: string;
-    // private id: string;
-    static fiscalYear = 2023;
-    protected employees: string[] = [];
+// type AddFm = (a: number, b: number) => void;
+interface AddFm {
+    (a: number, b: number): number;
+}
 
-    constructor (protected readonly id: string, public name: string) { // shorthand init
-        // this.name = n;
-        // this.id = id;
+
+let addFm: AddFm;
+
+addFm = (a: number, b: number) => {
+    return a + b;
+}
+
+// Defines the basic structure of object
+interface Named {
+    readonly name?: string;
+    optionalParam?: string;
+}
+interface Greetable extends Named {
+    greet(phrase: string): void
+}
+
+class Person implements Greetable {
+    name?: string;
+    age: number = 30;
+
+    constructor(n?: string) {
+        if(n) {
+            this.name = n;
+        }
     }
 
-    static createEmploy(name: string) {
-        return { name: name }
-    }
-
-    abstract describe(this: Department): void
-
-    addEmploye(employe: string) {
-        this.employees.push(employe);
-    }
-
-    employeesInfo() {
-        console.log(this.employees.length);
-        console.log(this.employees);
+    greet(phrase: string) {
+        if(this.name){
+            console.log(phrase + ' ' + this.name);
+        } else {
+            console.log('Hi!');
+        }
     }
 }
 
-class ITDepartment extends Department {
-    admins: string[];
-    constructor(id: string, admins: string[]) {
-        super(id, 'IT');
-        this.admins = admins
-    }
+let user1: Greetable;
 
-    describe() {
-        console.log('IT department ID: ', this.id);
-    }
-}
-
-class Accounting extends Department {
-    private lastReport: string;
-    private static instance: Accounting;
-
-    get mostRecentReport() {
-        if(this.lastReport){
-            return this.lastReport;
-        }
-        throw new Error('No repost found ...');
-    }
-
-    describe() {
-        console.log('Accounting department ID: ', this.id);
-    }
-
-    set mostRecentReport(value: string) {
-        if(!value) {
-            throw new Error('Please pass a valid value!')
-        }
-        this.addReport(value);
-    }
-
-    private constructor(id: string, private reports: string[]) {
-        super(id, 'Accounting');
-        this.lastReport = reports[0];
-    }
-
-    static getInstance () {
-        if(Accounting.instance){
-            return Accounting.instance;
-        }
-        this.instance = new Accounting('D2', []);
-        return this.instance;
-    }
-
-
-    addEmploye(name: string) {
-        if(name === 'Ravi') {
-            return;
-        }
-        this.employees.push(name);
-    }
-
-    addReport(text: string) {
-        this.reports.push(text);
-        this.lastReport = text;
-    }
-
-    printReports() {
-        console.log(this.reports);
-    }
-}
-
-// const department = new Department('D1', 'Accounting');
-// department.describe();
-
-// department.addEmploye('Ravi');
-// department.addEmploye('Verma');
-// // department.employees[2] = "Vivu"; It's a private
-// department.employeesInfo();
-
-// const departmentCopy = { name: 'DUMMY', describe: department.describe };
-// departmentCopy.describe();
-
-const employee = Department.createEmploy('Ravi');
-console.log(employee, Department.fiscalYear);
-
-//
-
-const ITDep = new ITDepartment('D1', ['Ravi']);
-
-console.log('Deparment Object: ', ITDep);
-
-ITDep.describe();
-
-ITDep.addEmploye('Ravi');
-ITDep.addEmploye('Verma');
-// deparment.employees[2] = "Vivu"; It's a private
-
-ITDep.employeesInfo();
-
-//
-
-// const accountDepartment = new Accounting('D2', []);
-const accountDepartment = Accounting.getInstance();
-
-// console.log(accountDepartment.mostRecentReport); // This will throw error
-// accountDepartment.mostRecentReport = '';  // This will throw error
-
-accountDepartment.mostRecentReport = 'Monthly reposrt';
-
-console.log(accountDepartment.mostRecentReport);
-// accountDepartment.addReport('test');
-// accountDepartment.printReports();
-accountDepartment.describe();
-
-accountDepartment.addEmploye('Ravi');
-accountDepartment.addEmploye('Verma');
-
-console.log(accountDepartment);
+user1 = new Person();
+// user1.name = 'Ravi'; // Cannot assign to 'name' because it is a read-only property
+user1.greet('My name is');
